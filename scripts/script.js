600c02dd349ad243a249655a5ad41a5b7383579a
Konstantin Vasilyev <hello.vasilyev@gmail.com>
2023-04-26 21:16:33 +0300

    правки после ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,44 @@
-let page = document.querySelector('.page');
-let form = page.querySelector('.popup');
-let close = page.querySelector('.button_close_popup');
-let open = document.querySelector('.button_edit_profile');
+let openProfileEditButton = document.querySelector('.profile__button-edit');
 
-let save = document.querySelector('.button_add_confirm');
+let closePopup = document.querySelector('.popup__button-close');
+let popups = document.querySelector('.popup')
+let editProfile = document.querySelector('.popup__edit-profile');
 
-let namePopup = form.querySelector('.input_name_field');
-let nameInPage = page.querySelector('.profile__names')
+let editProfileName = document.querySelector('.editProfileName');
+let profileNameInPage = document.querySelector('.profile__names');
 
-let jobPopup = form.querySelector('.input_job_field');
-let jobInPage = page.querySelector('.profile__job')
+let editProfileJob = document.querySelector('.editProfileJob');
+let profileJobInPage = document.querySelector('.profile__job');
 
-let like = document.querySelectorAll('.button_like_icon');
+let profileChangeSubmit = document.querySelector('.popup__button-submit');
 
-like.forEach(function(element) {
-  element.addEventListener('click', function() {
-    let likeIcon = element.querySelector('.icons_like_icon');
-    if (element.dataset.like === 'unchecked') {
-      likeIcon.src = 'images/like-fill-icon.svg';  
-      element.dataset.like = 'checked'; 
-      element.setAttribute('data-likecount', parseInt(element.dataset.likecount) + 1);
-    } else {
-      likeIcon.src = 'images/like-empty-icon.svg';  
-      element.dataset.like = 'unchecked';  
-      if (parseInt(element.dataset.likecount) >= 0) { 
-        element.setAttribute('data-likecount', parseInt(element.dataset.likecount) - 1);
-      }
-    }   
-  });
-});
 
 function openPopup(evt) {
-  evt.preventDefault();
-  namePopup.value = nameInPage.textContent;
-  jobPopup.value = jobInPage.textContent;
-  form.classList.add('popup_opened');
+  popups.classList.add('popup_opened');
+  let targetPopup = evt.target.classList;
+  switch (String(targetPopup)) {
+    case "profile__button-edit":
+      editProfile.classList.add('form_opened');
+      editProfileName.value = profileNameInPage.textContent;
+      editProfileJob.value = profileJobInPage.textContent;    
+    }       
 }
 
-function closePopup(evt) {
-  evt.preventDefault();
-  form.classList.remove('popup_opened');
+function closeAllPopup(evt) {
+  popups.classList.remove('popup_opened');
 }
 
 function changeNameAndJob(evt) {
   evt.preventDefault();
-  nameInPage.textContent = namePopup.value;
-  jobInPage.textContent = jobPopup.value;
-  closePopup(evt);
+  profileNameInPage.textContent = editProfileName.value;
+  profileJobInPage.textContent = editProfileJob.value;
+  closeAllPopup(evt);  
 }
 
-open.addEventListener('click', openPopup); 
-close.addEventListener('click', closePopup);
-save.addEventListener('click', changeNameAndJob);
+editProfile.addEventListener('submit', changeNameAndJob);
+openProfileEditButton.addEventListener('click', openPopup);
+closePopup.addEventListener('click', closeAllPopup);  
+
 
 
 
